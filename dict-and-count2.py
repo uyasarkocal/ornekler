604d2426e680612ba87f8c71ca26bc26034a5bff
Uğur Yaşar Koçal <uyasarkocal@gmail.com>
2015-02-01 11:53:42 +0200

    Kelime sayacı için bir diğer çözüm

---
+++ dict-and-count2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+dosya = open("/home/atarax/Desktop/deneme.txt", "r") 
+icerik = dosya.read().split()
+yaz = {}
+for kelime in icerik:
+    if kelime not in yaz:
+        yaz[kelime] = 1
+    else:
+        yaz[kelime] += 1
+for key in yaz.keys():
+   print ("%s %s " %(key , yaz[key] * '*'))
+
